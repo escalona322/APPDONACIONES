0f1851eefc56da511d468704d7233628a3dfed28
Jose Antonio <Jose Antonio@DESKTOP-OP3B5GE>
2020-05-06 15:54:39 +0200

    Codificadas las clases donacion, donante y formulario. Interfaces fxml diseñadas

--- src/Modelo/Donacion.java
+++ src/Modelo/Donacion.java
@@ -1,5 +1,75 @@
 package Modelo;
 
 public class Donacion {
+	private int Numdonacion;
+	private String Fecha;
+	private String TipoDonacion;
+	private String Pulso;
+	private String Ta_Sist;
+	private String HB_Cap;
+	private String HB_VEN;
+	private Number Volumen;
+	public Donacion(int numdonacion, String fecha, String tipoDonacion, String pulso, String ta_Sist, String hB_Cap,
+			String hB_VEN, Number volumen) {
+		super();
+		Numdonacion = numdonacion;
+		Fecha = fecha;
+		TipoDonacion = tipoDonacion;
+		Pulso = pulso;
+		Ta_Sist = ta_Sist;
+		HB_Cap = hB_Cap;
+		HB_VEN = hB_VEN;
+		Volumen = volumen;
+	}
+	public int getNumdonacion() {
+		return Numdonacion;
+	}
+	public void setNumdonacion(int numdonacion) {
+		Numdonacion = numdonacion;
+	}
+	public String getFecha() {
+		return Fecha;
+	}
+	public void setFecha(String fecha) {
+		Fecha = fecha;
+	}
+	public String getTipoDonacion() {
+		return TipoDonacion;
+	}
+	public void setTipoDonacion(String tipoDonacion) {
+		TipoDonacion = tipoDonacion;
+	}
+	public String getPulso() {
+		return Pulso;
+	}
+	public void setPulso(String pulso) {
+		Pulso = pulso;
+	}
+	public String getTa_Sist() {
+		return Ta_Sist;
+	}
+	public void setTa_Sist(String ta_Sist) {
+		Ta_Sist = ta_Sist;
+	}
+	public String getHB_Cap() {
+		return HB_Cap;
+	}
+	public void setHB_Cap(String hB_Cap) {
+		HB_Cap = hB_Cap;
+	}
+	public String getHB_VEN() {
+		return HB_VEN;
+	}
+	public void setHB_VEN(String hB_VEN) {
+		HB_VEN = hB_VEN;
+	}
+	public Number getVolumen() {
+		return Volumen;
+	}
+	public void setVolumen(Number volumen) {
+		Volumen = volumen;
+	}
+	
+	
 
 }
--- src/Modelo/Donante.java
+++ src/Modelo/Donante.java
@@ -1,5 +1,133 @@
 package Modelo;
 
 public class Donante {
+	private int N_Donante;
+	private String Cod_Postal;
+	private int Telefono;
+	private String Identificacion;
+	private String email;
+	private String Apellido1;
+	private String Apellido2;
+	private String Nombre;
+	private String Estado;
+	private String Fecha_Nac;
+	private String GrupoSang;
+	private String Ciclo;
+	
+	public Donante(int n_Donante, String cod_Postal, int telefono, String identificacion, String email,
+			String apellido1, String apellido2, String nombre, String estado, String fecha_Nac, String grupoSang,
+			String ciclo) {
+		super();
+		N_Donante = n_Donante;
+		Cod_Postal = cod_Postal;
+		Telefono = telefono;
+		Identificacion = identificacion;
+		this.email = email;
+		Apellido1 = apellido1;
+		Apellido2 = apellido2;
+		Nombre = nombre;
+		Estado = estado;
+		Fecha_Nac = fecha_Nac;
+		GrupoSang = grupoSang;
+		Ciclo = ciclo;
+	}
+
+	public int getN_Donante() {
+		return N_Donante;
+	}
+
+	public void setN_Donante(int n_Donante) {
+		N_Donante = n_Donante;
+	}
+
+	public String getCod_Postal() {
+		return Cod_Postal;
+	}
+
+	public void setCod_Postal(String cod_Postal) {
+		Cod_Postal = cod_Postal;
+	}
+
+	public int getTelefono() {
+		return Telefono;
+	}
+
+	public void setTelefono(int telefono) {
+		Telefono = telefono;
+	}
+
+	public String getIdentificacion() {
+		return Identificacion;
+	}
+
+	public void setIdentificacion(String identificacion) {
+		Identificacion = identificacion;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getApellido1() {
+		return Apellido1;
+	}
+
+	public void setApellido1(String apellido1) {
+		Apellido1 = apellido1;
+	}
+
+	public String getApellido2() {
+		return Apellido2;
+	}
+
+	public void setApellido2(String apellido2) {
+		Apellido2 = apellido2;
+	}
+
+	public String getNombre() {
+		return Nombre;
+	}
+
+	public void setNombre(String nombre) {
+		Nombre = nombre;
+	}
+
+	public String getEstado() {
+		return Estado;
+	}
+
+	public void setEstado(String estado) {
+		Estado = estado;
+	}
+
+	public String getFecha_Nac() {
+		return Fecha_Nac;
+	}
+
+	public void setFecha_Nac(String fecha_Nac) {
+		Fecha_Nac = fecha_Nac;
+	}
+
+	public String getGrupoSang() {
+		return GrupoSang;
+	}
+
+	public void setGrupoSang(String grupoSang) {
+		GrupoSang = grupoSang;
+	}
+
+	public String getCiclo() {
+		return Ciclo;
+	}
+
+	public void setCiclo(String ciclo) {
+		Ciclo = ciclo;
+	}
+	
+	
 
 }
