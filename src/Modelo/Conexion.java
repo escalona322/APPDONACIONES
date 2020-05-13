1101d89f045012e873099bb89399a40b7e11b337
Jose Antonio <Jose Antonio@DESKTOP-OP3B5GE>
2020-05-13 18:14:17 +0200

    Avances en el funcionamiento de donantes

--- src/Modelo/Conexion.java
+++ src/Modelo/Conexion.java
@@ -1,6 +1,7 @@
 package Modelo;
 	import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.sql.*;
@@ -8,6 +9,8 @@ import java.util.Properties;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 
 public class Conexion {
 
@@ -60,4 +63,301 @@ public class Conexion {
 			e.printStackTrace();
 		}
 	}
+	
+	//Insertar donante
+	public int InsertarDonante(String Cod_Postal, int Telefono, String ID, String email, 
+			String Apellido1, String Apellido2, String Nombre, String Estado, File foto,
+			String FechaNacimiento, String GrupoSang, String Ciclo) throws SQLException, FileNotFoundException{
+		
+		//Cuento los donantes para que NumDonantese pueda introducir automaticamente
+		String ContarDonantes = "SELECT N_Donante"+usr+"FROM DONANTE where N_Donante = (Select max(N_Donante) from "+usr+".Donante";
+		Statement stm = conexion.createStatement();
+		ResultSet cuenta = stm.executeQuery(ContarDonantes);
+		// Preparo la sentencia SQL
+		int SiguienteNumDonante = cuenta.getInt(1)+1;
+		String insertsql = "INSERT INTO "+usr+".DONANTE VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+
+		PreparedStatement pstmt = conexion.prepareStatement (insertsql);
+		pstmt.setInt(1, SiguienteNumDonante);
+		pstmt.setString(2, Cod_Postal);
+		pstmt.setInt(3, Telefono);
+		pstmt.setString(4, ID);
+		pstmt.setString(5, email);
+		pstmt.setString(6, Apellido1);
+		pstmt.setString(7, Apellido2);
+		pstmt.setString(8, Nombre);
+		pstmt.setString(9, Estado);
+		pstmt.setString(10, FechaNacimiento);
+		FileInputStream convertir_imagen = new FileInputStream (foto);
+		pstmt.setBlob(11, convertir_imagen, foto.length());
+		pstmt.setString(12, GrupoSang);
+		pstmt.setString(13, Ciclo);
+		//ejecuto la sentencia
+		try{
+			int resultado = pstmt.executeUpdate();
+
+			if(resultado != 1)
+				System.out.println("Error en la inserción " + resultado);
+			else
+				System.out.println("Persona insertada con éxito!!!");
+
+			return 0;
+		}catch(SQLException sqle){
+
+			int pos = sqle.getMessage().indexOf(":");
+			String codeErrorSQL = sqle.getMessage().substring(0,pos);
+
+			if(codeErrorSQL.equals("ORA-00001") ){
+				System.out.println("Ya existe una persona con  ese email!!");
+				return 1;
+			}
+			else{
+				System.out.println("Ha habido algún problema con  Oracle al hacer la insercion");
+				return 2;
+			}
+
+		}
+
+	}
+	
+	//modificar donante
+	public int ModificarDonante(String Nombre, String Apellido1, String Apellido2, String ID, 
+			String email, String GrupoSang, String Ciclo, int telefono,
+			String Cod_Postal, String FechaNacimiento) throws SQLException{
+
+
+		String updatesql = "UPDATE"+usr+".DONANTE SET Nombre=?, Apellido1=?, Apellido2=?, Identificacion=?, email=?, GrupoSang=?, Ciclo=?, Cod_Postal=?, Fecha_nacim=?, Telefono=? where Identificacion=?";
+
+		PreparedStatement pstmt = conexion.prepareStatement (updatesql);
+		pstmt.setString(1, Nombre);
+		pstmt.setString(2, Apellido1);
+		pstmt.setString(3, Apellido2);
+		pstmt.setString(4, ID);
+		pstmt.setString(5, email);
+		pstmt.setString(6, GrupoSang);
+		pstmt.setString(7, Ciclo);
+		pstmt.setString(8, Cod_Postal);
+		pstmt.setString(9, FechaNacimiento);
+		pstmt.setInt(10, telefono);
+		pstmt.setString(11, ID);
+
+
+		//ejecuto la sentencia
+		try{
+			int resultado = pstmt.executeUpdate();
+
+			if(resultado != 1)
+				System.out.println("Error en la actualización " + resultado);
+			else
+				System.out.println("Donante actualizada con éxito!!!");
+
+			return 0;
+		}catch(SQLException sqle){
+
+			int pos = sqle.getMessage().indexOf(":");
+			String codeErrorSQL = sqle.getMessage().substring(0,pos);
+
+			if(codeErrorSQL.equals("ORA-00001") ){
+				System.out.println("Ya existe un donante con ese id!!");
+				return 1;
+			}
+			else{
+				System.out.println("Ha habido algún problema con Oracle al hacer la insercion");
+				return 2;
+			}
+
+		}
+
+	}
+	
+	public ObservableList<Donante> ObtenerDonantes() throws SQLException{
+
+		ObservableList<Donante> listadonantes = FXCollections.observableArrayList();
+
+		//Preparo la conexión para ejecutar sentencias SQL de tipo update
+		Statement stm = conexion.createStatement();
+
+		// Preparo la sentencia SQL CrearTablaPersonas
+		String selectsql = "SELECT * FROM " + usr +".DONANTE";
+
+		//ejecuto la sentencia
+		try{
+			ResultSet resultado = stm.executeQuery(selectsql);
+
+			int contador = 0;
+			while(resultado.next()){
+				contador++;
+
+				int N_Donante= resultado.getInt(1);
+				String Cod_Postal = resultado.getString(2);
+				int telefono = resultado.getInt(3);
+				String identificacion = resultado.getString(4);
+				String email = resultado.getString(5);
+				String apellido1 = resultado.getString(6);
+				String apellido2 = resultado.getString(7);
+				String nombre = resultado.getString(8);
+				String estado = resultado.getString(9);
+				String fecha_Nac = resultado.getString(10);
+				String grupoSang = resultado.getString(11);
+				String ciclo = resultado.getString(12);
+				
+
+				Donante nueva = new Donante(N_Donante, Cod_Postal, telefono, identificacion, email,
+						apellido1, apellido2, nombre, estado, fecha_Nac, grupoSang,
+						ciclo);
+				listadonantes.add(nueva);
+			}
+
+			if(contador==0)
+				System.out.println("no data found");
+
+		}catch(SQLException sqle){
+
+			int pos = sqle.getMessage().indexOf(":");
+			String codeErrorSQL = sqle.getMessage().substring(0,pos);
+
+			System.out.println(codeErrorSQL);
+		}
+
+		return listadonantes;
+	}
+	
+	public int BorrarDonante(String email) throws SQLException{
+
+		// Preparo la sentencia SQL y la conexión para ejecutar sentencias SQL de tipo update
+		String deletesql = "DELETE " + usr +".DONANTE WHERE EMAIL= ?";
+		PreparedStatement pstmt = conexion.prepareStatement (deletesql);
+		pstmt.setString(1, email);
+
+		//ejecuto la sentencia
+		try{
+			int resultado = pstmt.executeUpdate();
+
+			if(resultado != 1)
+				System.out.println("Error en el borrado " + resultado);
+			else
+				System.out.println("Donante borrado con éxito!!!");
+
+			return 0;
+		}catch(SQLException sqle){
+
+			int pos = sqle.getMessage().indexOf(":");
+			String codeErrorSQL = sqle.getMessage().substring(0,pos);
+
+			System.out.println("Ha habido algún problema con  Oracle al hacer el borrado" + codeErrorSQL);
+			return 2;
+		}
+
+	}
+	
+	public ObservableList<Donante> BuscarDonantes(String Nombre, String Apellido, String email, String Identificacion, String telefono, String ciclo) throws SQLException{
+
+		ObservableList<Donante> listadonantes = FXCollections.observableArrayList();
+		PreparedStatement pstmt;
+
+
+		/*
+		 * Gracias a estos ifs hay un orden de prioridad al realizar la busqueda de donantes,
+		 * al escribir varios campos hay un orden de prioridad y el select que devuelve sera segun este checkeo.
+		 * Tambien se contara al usuario como es ese orden si se usa.
+		 */
+		String selectsql = "";
+		if(Identificacion.equals("")){
+			if(email.equals("")){
+				if(telefono.equals("")){
+					if(Nombre.equals("")){
+						if(Apellido.equals("")){
+							selectsql = "SELECT * FROM " + usr +".DONANTE";
+							pstmt = conexion.prepareStatement (selectsql);
+						}
+						else{
+							selectsql = "SELECT * FROM " + usr +".PERSONAS WHERE Apellido1 LIKE ?%";
+							pstmt = conexion.prepareStatement (selectsql);
+						}
+					}
+					else{
+						selectsql = "SELECT * FROM " + usr +".PERSONAS WHERE Nombre LIKE ?%";
+						pstmt = conexion.prepareStatement (selectsql);
+
+					}
+				}
+				else{
+					selectsql = "SELECT * FROM " + usr +".PERSONAS WHERE telefono LIKE ?%";
+					pstmt = conexion.prepareStatement (selectsql);
+
+				}
+			}
+			else{
+				selectsql = "SELECT * FROM " + usr +".PERSONAS WHERE email LIKE ?%";
+				pstmt = conexion.prepareStatement (selectsql);
+			}
+		}
+		else{
+			selectsql = "SELECT * FROM " + usr +".PERSONAS WHERE Identificacion LIKE ?%";
+			pstmt = conexion.prepareStatement (selectsql);
+			
+			Alert alert = new Alert(AlertType.INFORMATION);
+			alert.setTitle("Aviso!");
+			alert.setHeaderText("Se ha usado un orden de prioridad!!");
+			alert.setContentText("Al buscar por varios campos se busca con este orden: ID>Email>Tlfno>Nombre>Apellido!");
+			alert.showAndWait();
+		}
+
+
+
+		//ejecuto la sentencia
+		try{
+			ResultSet resultado = pstmt.executeQuery();
+
+			int contador = 0;
+			while(resultado.next()){
+				contador++;
+
+
+				int N_Donante= resultado.getInt(1);
+				String Cod_Postal = resultado.getString(2);
+				int telefono2 = resultado.getInt(3);
+				String identificacion = resultado.getString(4);
+				String email2 = resultado.getString(5);
+				String apellido1 = resultado.getString(6);
+				String apellido2 = resultado.getString(7);
+				String nombre = resultado.getString(8);
+				String estado = resultado.getString(9);
+				String fecha_Nac = resultado.getString(10);
+				String grupoSang = resultado.getString(11);
+				String ciclo2 = resultado.getString(12);
+
+				Donante nueva = new Donante(N_Donante, Cod_Postal, telefono2, identificacion, email2,
+						apellido1, apellido2, nombre, estado, fecha_Nac, grupoSang,
+						ciclo2);
+				listadonantes.add(nueva);
+			}
+
+			if(contador==0)
+				System.out.println("no data found");
+
+		}catch(SQLException sqle){
+
+			int pos = sqle.getMessage().indexOf(":");
+			String codeErrorSQL = sqle.getMessage().substring(0,pos);
+
+			System.out.println(codeErrorSQL);
+		}
+
+		return listadonantes;
+	}
+	
+	public byte[] LeerFoto(String Identificacion) throws SQLException{
+		
+		byte[] byteImage = null;
+		
+		try{
+			//Sentencia sql
+			String selectfoto = "SELECT "
+		}catch(SQLException sqle){
+			
+		}
+		
+	}
+
 }
--- src/Vista/ControladoraDonantes.java
+++ src/Vista/ControladoraDonantes.java
@@ -1,12 +1,24 @@
 package Vista;
 
+import java.io.File;
+import java.sql.SQLException;
+
+import Modelo.Conexion;
 import Modelo.Donante;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.DatePicker;
+import javafx.scene.control.RadioButton;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleGroup;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.image.ImageView;
 
 public class ControladoraDonantes {
@@ -36,7 +48,19 @@ public class ControladoraDonantes {
 	private TextField CodigoPostal;
 	
 	@FXML
-	private TextField GrupoSanguineo;
+	private ToggleGroup GrupoS;
+	
+	@FXML
+	private RadioButton A;
+	
+	@FXML
+	private RadioButton B;
+	
+	@FXML
+	private RadioButton AB;
+	
+	@FXML
+	private RadioButton Cero;
 	
 	@FXML
 	private DatePicker FechaNacimiento;
@@ -98,8 +122,137 @@ public class ControladoraDonantes {
 	@FXML
 	private TableColumn<Donante,String> TelefonoT;
 	
+	ObservableList<Donante> datos = FXCollections.observableArrayList();
+	
+	private boolean edicion;
+	private int indiceedicion;
+	
+
+	public void initialize() throws SQLException{
+
+		// Llamar a un método de la clase de manipulación de BBDD para que me devuelva un ObservableList<Persona> datos
+
+		Conexion con = new Conexion();
+		datos = con.ObtenerDonantes();
+
+		Tabla.setItems(datos);
+
+		NombreT.setCellValueFactory(new PropertyValueFactory<Donante,String>("Nombre"));
+		ApellidoT.setCellValueFactory(new PropertyValueFactory<Donante,String>("Apellido1"));
+		emailT.setCellValueFactory(new PropertyValueFactory<Donante,String>("email"));
+		GSangT.setCellValueFactory(new PropertyValueFactory<Donante,String>("GrupoSang"));
+		TelefonoT.setCellValueFactory(new PropertyValueFactory<Donante,String>("Telefono"));
+
+		// Al arrancar la vista se pone edicion a false
+		edicion = false;
+		indiceedicion = 0;
+
+	}
+	
 	public void InsertarDonante(){
-		
+
+		// Añadir un chequeo de campos vacíos
+		if(Nombre.getText().equals("") || Apellido1.getText().equals("") || Apellido2.getText().equals("") || email.getText().equals("") || Identificacion.getText().equals("")|| Telefono.getText().equals("") || Ciclo.getText().equals("") || CodigoPostal.getText().equals("")){
+			Alert alert = new Alert(AlertType.ERROR);
+			alert.setTitle("Error!!!");
+			alert.setHeaderText("Observa que hayas introducido todos los datos");
+			alert.setContentText("¡No se pueden grabar campos vacíos!");
+			alert.showAndWait();
+		}
+		else{
+
+			if(edicion == true){
+			
+				// Hago la llamda al método que hace el update en la base de datos
+				Conexion con = new Conexion();
+				String GrupoSang = "";
+				if(A.isSelected()){
+					GrupoSang = "A";
+				}
+				else{
+					if(B.isSelected()){
+						GrupoSang = "B";
+					}else{
+						if(AB.isSelected()){
+							GrupoSang = "AB";
+						}else{
+							if(Cero.isSelected()){
+								GrupoSang = "0";
+							}
+					}
+					}
+				}
+				
+				int res = con.ModificarDonante(Nombre.getText(), Apellido1.getText(), Apellido2.getText(), Identificacion.getText(), email.getText(), GrupoSang, Ciclo.getText(), Integer.parseInt(Telefono.getText()), CodigoPostal.getText(), FechaNacimiento.getValue().toString());
+				switch (res){
+
+					case 0:
+						Alert alert = new Alert(AlertType.INFORMATION);
+						alert.setTitle("OK!");
+						alert.setHeaderText("Modificación OK!");
+						alert.setContentText("¡Persona modificada con éxito!");
+						alert.showAndWait();
+
+						// Actualizo los datos de la tabla
+						datos = con.ObtenerDonantes();
+						Tabla.setItems(datos);
+						break;
+
+					default:
+							alert = new Alert(AlertType.ERROR);
+							alert.setTitle("Error!");
+							alert.setHeaderText("Modificación NOK!");
+							alert.setContentText("¡Ha habido un problema al realizar el update!");
+							alert.showAndWait();
+							break;
+
+						}
+
+
+
+
+			}
+			else{
+				// Realizar el insertado de datos en la base de datos
+				Conexion con = new Conexion();
+				File foto = new File();
+				int res = con.InsertarDonante(CodigoPostal.getText(), Integer.parseInt(Telefono.getText()), Identificacion.getText(), email.getText(), Apellido1.getText(), Apellido2.getText(), Nombre.getText(), null, foto, FechaNacimiento.getValue().toString(), GrupoSang, Ciclo.getText());
+				switch (res){
+
+				case 0:
+					Alert alert = new Alert(AlertType.INFORMATION);
+					alert.setTitle("OK!");
+					alert.setHeaderText("Inserción OK!");
+					alert.setContentText("¡Persona insertada con éxito!");
+					alert.showAndWait();
+
+					// Actualizo los datos de la tabla
+					datos = con.ObtenerDonantes();
+					Tabla.setItems(datos);
+					break;
+
+				case 1:
+					alert = new Alert(AlertType.WARNING);
+					alert.setTitle("Aviso!");
+					alert.setHeaderText("Inserción NOK!");
+					alert.setContentText("¡Ya existe una persona con ese email!");
+					alert.showAndWait();
+					break;
+
+				default:
+					alert = new Alert(AlertType.ERROR);
+					alert.setTitle("Error!");
+					alert.setHeaderText("Inserción NOK!");
+					alert.setContentText("¡Ha habido un problema al realizar la inserción!");
+					alert.showAndWait();
+					break;
+
+				}
+
+			}
+
+		}
+
 	}
 	
 	public void ModificarDonante(){
